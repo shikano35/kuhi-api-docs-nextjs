f4f0c740620c95aef0b3db8b50668ee8bd74a8fb
shikano <pegtar377512@gmail.com>
2025-02-21 21:48:21 +0900

    feat: カードコンポーネントを追加（#11）

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,23 @@
+import styles from './index.module.scss';
+
+export function Card({
+  title,
+  description,
+  href,
+  icon: Icon,
+}: {
+  title: string;
+  description: string;
+  href: string;
+  icon: React.FC<React.SVGProps<SVGSVGElement>>;
+}) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.card__header}>
+        <Icon className={styles.card__icon} />
+        <h3 className={styles.card__title}>{title}</h3>
+      </div>
+      <p className={styles.card__description}>{description}</p>
+    </div>
+  );
+}
