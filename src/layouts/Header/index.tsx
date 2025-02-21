d27b4744f602080ce34ba9f9122e496ee4019dae
shikano <pegtar377512@gmail.com>
2025-02-21 12:17:38 +0900

    fix: ヘッダーコンポーネントのスタイルと構造を更新（#4）

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,12 +1,20 @@
+import Link from 'next/link';
+import { Roboto_Slab }from 'next/font/google'
 import styles from './index.module.scss';
+
+import clsx from 'clsx';
+import { ExternalLink } from '@/components/Link';
+
+const robotoSlab = Roboto_Slab({ subsets: ['latin'] })
 
 export function Header() {
   return (
-    <header>
-      <div>
-        <h1 className={styles.h1}>
-          Sample
-        </h1>
+    <header className={styles.header}>
+      <div className={styles.header__inner}>
+        <Link href="/" className={styles.header__title}>
+          <span className={clsx(robotoSlab.className)}>HaikuMonument API</span>
+        </Link>
+        <ExternalLink href="https://api.kuhi.jp/docs" label="Swagger documents" />
       </div>
     </header>
   );
