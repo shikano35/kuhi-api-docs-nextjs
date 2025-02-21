51e02332b7fb6127272662ac4b85ceeee3ec08d8
shikano <pegtar377512@gmail.com>
2025-02-21 16:58:34 +0900

    fix: フッターコンポーネントのスタイルと構造を更新（#4）

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,20 +1,38 @@
-import { Logo } from "@/components/Logo";
+import Link from "next/link";
+import { Roboto_Slab }from 'next/font/google'
 import styles from "./index.module.scss";
+import { navItems } from "../Nav";
+import { NavItem } from "../Nav/NavItem";
+
+const robotoSlab = Roboto_Slab({ subsets: ['latin'] })
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
-    <footer className={styles.footer}>
-      <div className={styles.footer__inner}>
-        <span>
-          &copy; {new Date().getFullYear()} {" "}
-          <a href="https://shikano35.com" >
-            shikano35
+    <footer className={styles.footer} aria-label="フッター"> 
+    <div className={styles.footer__inner}>
+      <div className={styles.footer__description}>
+      <Link href="/" className={styles.footer__title} aria-label="ホームページ">
+        <span className={robotoSlab.className}>HaikuMonument API</span>
+      </Link>
+      <p>句碑APIは、句碑のデータを提供するAPIです。</p>
+      <small className={styles.footer__copyright}>
+          &copy; {currentYear}{" "}
+          <a href="https://github.com/shikano35" className={styles.footer__copyright__link} target="_blank" rel="noopener noreferrer">
+            Shikano35
           </a>
-        </span>
-        <div>
-          <p>test</p>
-        </div>
+        </small>
       </div>
-    </footer>
+      <nav className={styles.footer__navlist} aria-label="Footer Navigation">
+      <ul className={styles.footer__navlist__list}>
+        {navItems.map((item) => (
+          <li key={item.title} className={styles.footer__navlist__item}>
+            <NavItem title={item.title} label={item.label} href={item.href} />
+          </li>
+        ))}
+      </ul>
+    </nav>
+    </div>
+  </footer>
   );
 }
