bbb25395e381aa5270900b7202e1b41a710b580d
shikano <pegtar377512@gmail.com>
2025-02-21 12:16:34 +0900

    fix: リンクコンポーネントを更新（#4）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { DetailLink } from "@/components/Link";
 import styles from "./index.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <DetailLink href="/" message="sample" />
       HomePage
     </div>
   );
