7ba4e6f6194f2695e2e8528baf8f71241b2d886a
shikano <pegtar377512@gmail.com>
2025-02-21 14:20:14 +0900

    fix: ヘッダーコンポーネントの完成（#4）

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 import { Roboto_Slab }from 'next/font/google'
 import styles from './index.module.scss';
-
-import clsx from 'clsx';
 import { ExternalLink } from '@/components/Link';
 
 const robotoSlab = Roboto_Slab({ subsets: ['latin'] })
@@ -11,8 +9,8 @@ export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.header__inner}>
-        <Link href="/" className={styles.header__title}>
-          <span className={clsx(robotoSlab.className)}>HaikuMonument API</span>
+        <Link href="/" className={styles.header__title} aria-label="ホームページ">
+          <span className={robotoSlab.className}>HaikuMonument API</span>
         </Link>
         <ExternalLink href="https://api.kuhi.jp/docs" label="Swagger documents" />
       </div>
