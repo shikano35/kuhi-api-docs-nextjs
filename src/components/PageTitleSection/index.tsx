ee1e641f182c6d5503fa14152de0ec90e524b8c3
shikano <pegtar377512@gmail.com>
2025-02-23 10:36:12 +0900

    fix: ページタイトルセクションコンポーネントに分割

---
+++ src/components/PageTitleSection/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./index.module.scss";
+
+export default function PageTitleSection() {
+  return (
+    <section className={styles.pageTitle}>
+      <header className={styles.pageTitle__inner}>
+        <h1 className={styles.pageTitle__title}>
+          HaikuMonument API Documentation
+        </h1>
+        <p className={styles.pageTitle__subtitle}>
+          句碑API ドキュメント
+        </p>
+      </header>
+    </section>
+  );
+}
