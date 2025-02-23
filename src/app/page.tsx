d97f70d19d7d75c8df3104d6ee7631e2614753a1
shikano <pegtar377512@gmail.com>
2025-02-23 10:01:25 +0900

    refactor: コンポーネントの構造を整理

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { Card } from "@/components/Card";
 import styles from "./index.module.scss";
-import { BuildingLibraryIcon, UserIcon, MapPinIcon, BookOpenIcon } from "@heroicons/react/24/outline";
+import {
+  BuildingLibraryIcon,
+  UserIcon,
+  MapPinIcon,
+  BookOpenIcon,
+} from "@heroicons/react/24/outline";
 import { EndpointsSection } from "@/components/EndpointsSection";
 import { haikuMonuments, poets, locations, sources } from "@/lib/data";
 import { ChangelogSection } from "@/components/ChangelogSection";
@@ -8,7 +13,8 @@ import { ChangelogSection } from "@/components/ChangelogSection";
 const apiList = [
   {
     title: "句碑",
-    description: "建立された場所、俳句の作者、句の内容などの情報を取得することができます",
+    description:
+      "建立された場所、俳句の作者、句の内容などの情報を取得することができます",
     item: BuildingLibraryIcon,
   },
   {
@@ -25,29 +31,39 @@ const apiList = [
     title: "出典",
     description: "句碑の出典情報を取得することができます",
     item: BookOpenIcon,
-  }
+  },
 ];
 
 export default function Home() {
   return (
     <div className={styles.layout}>
       <section className={styles.section__pagetitle}>
-        <div className={styles.section__pagetitle__inner}>
-          <h1 className={styles.section__pagetitle__title}>HaikuMonument API Documentation</h1>
-          <small className={styles.section__pagetitle__subtitle}>句碑API ドキュメント</small>
-        </div>
+        <header className={styles.section__pagetitle__inner}>
+          <h1 className={styles.section__pagetitle__title}>
+            HaikuMonument API Documentation
+          </h1>
+          <p className={styles.section__pagetitle__subtitle}>
+            句碑API ドキュメント
+          </p>
+        </header>
       </section>
-      <div className={styles.border} />
+
+      <hr className={styles.border} />
+
       <section className={styles.section__overview} id="overview">
         <div className={styles.section__overview__inner}>
-          <h2 className={styles.section__overview__title}>Overview</h2>
-          <small className={styles.section__overview__subtitle}>概要</small>
+          <header>
+            <h2 className={styles.section__overview__title}>Overview</h2>
+            <p className={styles.section__overview__subtitle}>概要</p>
+          </header>
           <p className={styles.section__overview__description}>
             このAPIは、句碑の情報を提供します
             <br />
             以下の情報を取得することができます
           </p>
-          <p className={styles.section__overview__destructive}>※ 現在制作中です</p>
+          <p className={styles.section__overview__destructive}>
+            ※ 現在制作中です
+          </p>
         </div>
         <div className={styles.section__overview__content}>
           <ul className={styles.section__overview__list}>
@@ -63,11 +79,17 @@ export default function Home() {
           </ul>
         </div>
       </section>
-      <div className={styles.border} />
+
+      <hr className={styles.border} />
+
       <section className={styles.section__reference} id="reference">
         <div className={styles.section__reference__inner}>
-          <h2 className={styles.section__reference__title}>API Reference</h2>
-          <small className={styles.section__reference__subtitle}>APIリファレンス</small>
+          <header>
+            <h2 className={styles.section__reference__title}>API Reference</h2>
+            <p className={styles.section__reference__subtitle}>
+              APIリファレンス
+            </p>
+          </header>
         </div>
         <p className={styles.section__reference__description}>
           このAPIは、以下のエンドポイントを提供します
@@ -88,9 +110,10 @@ export default function Home() {
         <EndpointsSection title="/locations" endpoints={locations} />
         <EndpointsSection title="/sources" endpoints={sources} />
       </section>
-      <div className={styles.border} />
+
+      <hr className={styles.border} />
+
       <ChangelogSection />
-      <div className={styles.border} />
     </div>
   );
 }
